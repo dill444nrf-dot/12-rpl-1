81ccb98f8f4d9cb8c7a901e0349ee909116f8b50
dill444nrf-dot <dill444nrf@gmail.com>
2026-08-05 12:22:53 +0700

    tugas auth

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/profile', [AuthController::class, 'profile']);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+});
